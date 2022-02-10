32cd6c9db75b496ff48494f8c61dc829f794d506
PraveenAndrew <84122399+PraveenAndrew@users.noreply.github.com>
2022-02-10 19:25:40 +0530

    Add files via upload
    simple survey projects related php and mysql

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>online survey</title>
+</head>
+<body>
+
+	
+		
+
+		<form action="connect.php" method="POST">
+      
+       <h1> Online Surveys for Mobile Phone </h1>
+
+     
+        
+        <h3> <bold>Redmi Phone </bold></h3> 
+
+          <input type="radio"  name="redmi" value="1">poor
+          <input type="radio"  name="redmi" value="2">Good
+          <input type="radio"  name="redmi" value="3">Very Good
+          <input type="radio"  name="redmi" value="4">Excellent
+          <input type="radio"  name="redmi" value="5">very Excellent 
+      
+
+       
+        
+        <h3> <bold>Samsung Phone </bold></h3> 
+
+          <input type="radio"  name="samsung" value="1">poor
+          <input type="radio"  name="samsung" value="2">Good
+          <input type="radio"  name="samsung" value="3">Very Good
+          <input type="radio"  name="samsung" value="4">Excellent
+          <input type="radio"  name="samsung" value="5"> very Excellent
+       
+        
+        <h3> <bold>Realme Phone </bold></h3> 
+
+          <input type="radio"  name="realme" value="1">poor
+          <input type="radio"  name="realme" value="2">Good
+          <input type="radio"  name="realme" value="3">Very Good
+          <input type="radio"  name="realme" value="4">Excellent
+          <input type="radio"  name="realme" value="5"> very Excellent
+      
+
+
+	
+
+   <input type="submit" value="submit" >
+
+</form>
+
+</body>
+</html>
